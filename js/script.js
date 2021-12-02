54c4340dc51020fa85fb174fe7e16d5fe017f61e
Lone Mortensen <lmordev@gmail.com>
2021-12-02 15:44:18 -0800

    Added functions to check and capture player input

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // GUESS THE WORD GAME
 
 //List of all of player's guessed letters:
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElements = document.querySelector(".guessed-letters");
 //Guess! button:
 const guessButton = document.querySelector(".guess");
 //Field where player inputs letter:
@@ -16,9 +16,10 @@ const numberGuesses = document.querySelector("span");
 const playerMessage = document.querySelector(".message");
 //Play Again! button:
 const playAgainButton = document.querySelector(".play-again");
-
 //Word the player must guess:
 const word = "magnolia";
+//Letters the player guesses:
+const guessedLetters = [];
 
 //Add placeholder circles for each letter in word:
 const addPlaceholders = function (word) {
@@ -32,15 +33,52 @@ const addPlaceholders = function (word) {
 
 addPlaceholders(word);
 
-//Eventlistener for Guess button:
+//Eventlistener for Guess! button and validation of player's input:
 guessButton.addEventListener("click", function(e) {
-    e.preventDefault(); // prevents reloading of page after submitting the form.
+    e.preventDefault(); // prevents reloading of page after submitting form.
+    clearPlayerMessage();
     const letter = letterInput.value; 
-    console.log(letter);
+    //console.log(letter);
+    const playerGuess = checkInput(letter); // saves result of checkInput function call to variable.
+    if (playerGuess) {
+        makeGuess(letter);
+    } 
+    //console.log(playerGuess);
     clearInput();
 }); 
 
-//Function to clear letter input field when clicking the Guess button:
+//Clear letter input field when clicking the Guess! button:
 const clearInput = function () {
     letterInput.value = "";
 };
+
+//Validate if player's input is a letter:
+const checkInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/; // regular expression ensures player's input is a letter.
+    if (input.length === 0) {
+        playerMessage.innerText = "Please enter your guess.";
+    } else if (input.length > 1) {
+        playerMessage.innerText = "You can only enter one letter at a time.";
+    } else if (!input.match(acceptedLetter)) {
+        playerMessage.innerText = "Please enter a letter from A to Z only.";
+    } else {
+        return input;
+    }
+};
+
+//Clear player message field when clicking the Guess! button:
+const clearPlayerMessage = function () {
+    playerMessage.innerText = "";
+};
+
+//Check if player has already guessed the letter:
+const makeGuess = function(letter) {
+    letter = letter.toUpperCase();
+    if (guessedLetters.includes(letter)) { 
+        playerMessage.innerText = "You already guessed that letter. Try again.";
+    } else {
+        guessedLetters.push(letter);
+    }
+    console.log(guessedLetters);
+};  
+
